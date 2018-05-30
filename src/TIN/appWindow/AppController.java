b492acb344ced77d3f4470a1f5e3c6b12bd2efbb
tmurawsk <tmurawsk@mion.elka.pw.edu.pl>
2018-05-30 13:57:23 +0200

    GUI basics

--- src/TIN/appWindow/AppController.java
+++ src/TIN/appWindow/AppController.java
@@ -1,4 +1,47 @@
 package TIN.appWindow;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class AppController {
+    @FXML
+    private Text filePathText;
+
+    @FXML
+    private Button sendButton;
+    @FXML
+    private Button loadButton;
+    @FXML
+    private Button disconnectButton;
+
+    @FXML
+    private void onDisconnectButtonClicked() {
+        Stage stage = (Stage) disconnectButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void onLoadButtonClicked() {
+
+    }
+
+    @FXML
+    private void onSendButtonClicked() {
+        String filePath = filePathText.getText();
+
+        if(!validateFilePathText(filePath)){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No file to send");
+            alert.setContentText("Please choose a file before sending!");
+            alert.showAndWait();
+        }
+    }
+
+    private boolean validateFilePathText(String filePath) {
+        return !filePath.equals("*none*");
+    }
 }
